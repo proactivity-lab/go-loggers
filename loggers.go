835309e513b53bca240ad421ad94d90fc8a6a61c
Raido Pahtma <raidopahtma@gmail.com>
2015-04-24 12:21:59 +0300

    Initial version.

---
+++ loggers.go
@@ -0,0 +1,44 @@
+// Author  Raido Pahtma
+// License MIT
+
+package loggers
+
+import "log"
+import "io/ioutil"
+
+type DIWElog interface {
+	SetDebugLogger(logger *log.Logger)
+	SetInfoLogger(logger *log.Logger)
+	SetWarningLogger(logger *log.Logger)
+	SetErrorLogger(logger *log.Logger)
+}
+
+type DIWEloggers struct {
+	Debug   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+}
+
+func (self *DIWEloggers) InitLoggers() {
+	self.Debug = log.New(ioutil.Discard, "", 0)
+	self.Info = log.New(ioutil.Discard, "", 0)
+	self.Warning = log.New(ioutil.Discard, "", 0)
+	self.Error = log.New(ioutil.Discard, "", 0)
+}
+
+func (self *DIWEloggers) SetDebugLogger(logger *log.Logger) {
+	self.Debug = logger
+}
+
+func (self *DIWEloggers) SetInfoLogger(logger *log.Logger) {
+	self.Info = logger
+}
+
+func (self *DIWEloggers) SetWarningLogger(logger *log.Logger) {
+	self.Warning = logger
+}
+
+func (self *DIWEloggers) SetErrorLogger(logger *log.Logger) {
+	self.Error = logger
+}
