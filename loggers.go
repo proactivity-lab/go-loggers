f892709079bd495044b2ce801c6993389f00dd9e
Raido Pahtma <raidopahtma@gmail.com>
2018-04-17 11:58:28 +0300

    Add BasicLogSetup.

--- loggers.go
+++ loggers.go
@@ -5,12 +5,14 @@ package loggers
 
 import "log"
 import "io/ioutil"
+import "os"
 
 type DIWElog interface {
 	SetDebugLogger(logger *log.Logger)
 	SetInfoLogger(logger *log.Logger)
 	SetWarningLogger(logger *log.Logger)
 	SetErrorLogger(logger *log.Logger)
+	SetLoggers(loggers *DIWEloggers)
 }
 
 type DIWEloggers struct {
@@ -54,4 +56,23 @@ func (self *DIWEloggers) SetLoggers(loggers *DIWEloggers) {
 	self.Info = loggers.Info
 	self.Warning = loggers.Warning
 	self.Error = loggers.Error
+}
+
+func BasicLogSetup(debuglevel int) *DIWEloggers {
+	logger := New()
+	logformat := log.Ldate | log.Ltime | log.Lmicroseconds
+
+	if debuglevel > 1 {
+		logformat = logformat | log.Lshortfile
+	}
+
+	if debuglevel > 0 {
+		logger.SetDebugLogger(log.New(os.Stdout, "DEBUG: ", logformat))
+		logger.SetInfoLogger(log.New(os.Stdout, "INFO:  ", logformat))
+	} else {
+		logger.SetInfoLogger(log.New(os.Stdout, "", logformat))
+	}
+	logger.SetWarningLogger(log.New(os.Stdout, "WARN:  ", logformat))
+	logger.SetErrorLogger(log.New(os.Stdout, "ERROR: ", logformat))
+	return logger
 }
